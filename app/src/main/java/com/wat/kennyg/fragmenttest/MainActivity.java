721c764c4a0124e646adc5f880007212bd84da4b
Kenny <kguo6@my.bcit.ca>
2018-03-12 14:59:02 -0700

    Fragment tutorial

--- app/src/main/java/com/wat/kennyg/fragmenttest/MainActivity.java
+++ app/src/main/java/com/wat/kennyg/fragmenttest/MainActivity.java
@@ -1,13 +1,21 @@
 package com.wat.kennyg.fragmenttest;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MyListFragment.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onRssItemSelected(String text) {
+        DetailFragment fragment = (DetailFragment) getFragmentManager()
+                .findFragmentById(R.id.detailFragment);
+        fragment.setText(text);
     }
 }
